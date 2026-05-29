4a2b28ab43307296742d85e46fd3f7a0cbac5b68
sachhsin baghel <sachhsinbaghel@gmail.com>
2026-05-29 22:17:56 +0530

    very frustated today

--- JavaOnly/Basics.java
+++ JavaOnly/Basics.java
@@ -1,10 +1,14 @@
 import java.util.*;
-public class Basics {
-public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
-int A = sc.nextInt();
-int B = sc.nextInt();
-int sum = A + B;
-System.out.println("The sum of A and B is: " + sum);
-}
+public class Basics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Type First number : ");
+        
+        int a = sc.nextInt();
+        System.out.print("Type Second number : ");
+     
+        int b = sc.nextInt();
+        System.out.println("Sum: " + (a+b));
+        sc.close();
+    }
 }
