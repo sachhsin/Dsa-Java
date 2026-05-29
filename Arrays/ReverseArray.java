4c8aa40a5b9d6b9667fab7e5a9e3978cd0238931
sachhsin baghel <sachhsinbaghel@gmail.com>
2026-05-29 12:05:04 +0530

    reverse array done meri jaan

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,9 +1,9 @@
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr = {2,3,4,5,6,8};// first 
+        int[] arr = {1, 2, 3, 4, 5};
         
         int left = 0, right = arr.length - 1;
-        while (left < right) {
+        while (left < right) {//read for both and
             int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
